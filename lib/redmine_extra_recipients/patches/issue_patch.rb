d232ea6dc3525f17bf256bba3b898fb9a4fcef38
freedayko <freeday@apmmpc.dp.ua>
2010-11-24 19:17:07 +0200

    Add project and status selectors to plugin settings page.

--- lib/redmine_extra_recipients/patches/issue_patch.rb
+++ lib/redmine_extra_recipients/patches/issue_patch.rb
@@ -21,15 +21,26 @@ module RedmineExtraRecipients
           if Setting['plugin_redmine_extra_recipients'].present? &&
               Setting['plugin_redmine_extra_recipients']['recipients'].present?
 
-            if project.is_public || (!project.is_public && Setting['plugin_redmine_extra_recipients']['private_project_visibility'] == 'include_private')
-            
+            should_add_recipients = false;
+
+            if Setting['plugin_redmine_extra_recipients']['private_project_visibility'] != 'custom_projects'
+              should_add_recipients = project.is_public || (!project.is_public && Setting['plugin_redmine_extra_recipients']['private_project_visibility'] == 'include_private')
+            else
+              should_add_recipients = Setting['plugin_redmine_extra_recipients']['projects'].include?(project.id.to_s)
+            end
+
+            if Setting['plugin_redmine_extra_recipients']['new_status'].present?
+              should_add_recipients &&= Setting['plugin_redmine_extra_recipients']['new_status'].include?(status.id.to_s)
+            end
+
+            if  should_add_recipients
               comma_or_newline_regex = /[,\n]/
 
               extra_recipients = Setting['plugin_redmine_extra_recipients']['recipients']
               notified += extra_recipients.split(comma_or_newline_regex).collect(&:strip)
             end
           end
-          
+
           notified
         end
       end
